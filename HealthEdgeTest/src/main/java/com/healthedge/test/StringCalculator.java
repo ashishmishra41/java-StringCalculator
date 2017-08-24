43a73380919fa9e9a52db83389307e0324143466
ashishmishra41 <31226353+ashishmishra41@users.noreply.github.com>
2017-08-23 23:32:38 -0400

    Added implementation for custom delimiter.

--- HealthEdgeTest/src/main/java/com/healthedge/test/StringCalculator.java
+++ HealthEdgeTest/src/main/java/com/healthedge/test/StringCalculator.java
@@ -5,16 +5,23 @@ public class StringCalculator {
 	private String delimiter = ",|\n";
 
 	public int add(String input) {
-
-		if (input == null || input.isEmpty()) {
+		String numberToAdd = input;
+		String numbers[] = null;
+		if (input==null || input.isEmpty()) {
 			return 0;
 		}
-		return addNumbers(input.split(delimiter));
+		if (input.startsWith("//")) {
+			String[] delimWithNumber = input.split("\n", 2);
+			numberToAdd = delimWithNumber[1];
+			delimiter = delimWithNumber[0].substring(2) + "|\n";
+		}
+		numbers = numberToAdd.split(delimiter);
+		return addNumbers(numbers);
 	}
 
-	private int addNumbers(String[] numbers){
+	private int addNumbers(String[] numbers) {
 		int sumOfNumbers = 0;
-		for(String number : numbers){
+		for (String number : numbers) {
 			sumOfNumbers = sumOfNumbers + Integer.parseInt(number);
 		}
 		return sumOfNumbers;
