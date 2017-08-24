0fec0cdd058ddc7e1230334f89c421f0d0adf587
ashishmishra41 <31226353+ashishmishra41@users.noreply.github.com>
2017-08-23 23:04:58 -0400

    Added implementation for adding 2 numbers with "," as delimiter.

--- HealthEdgeTest/src/main/java/com/healthedge/test/StringCalculator.java
+++ HealthEdgeTest/src/main/java/com/healthedge/test/StringCalculator.java
@@ -2,10 +2,20 @@ package com.healthedge.test;
 
 public class StringCalculator {
 
-	public int add(String input){
-		if(input == null || input.isEmpty()){
+	private String delimiter = ",";
+
+	public int add(String input) {
+
+		if (input == null || input.isEmpty()) {
 			return 0;
 		}
-		return Integer.parseInt(input);
+		return addNumbers(input.split(delimiter));
+	}
+
+	private int addNumbers(String[] numbers) {
+		int sumOfNumbers = 0;
+		sumOfNumbers = numbers.length == 1 ? Integer.parseInt(numbers[0])
+				: (Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]));
+		return sumOfNumbers;
 	}
 }
